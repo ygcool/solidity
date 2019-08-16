8fe38e064ffe1445e44fa4d531cd4f60995f2f10
ygcool <764990304@qq.com>
2019-08-16 11:03:24 +0800

    Update erc20.sol

--- sol/erc20.sol
+++ sol/erc20.sol
@@ -13,14 +13,15 @@ contract ERC20Standard {
 	mapping (address => uint256) balances;
 	mapping (address => mapping (address => uint256)) allowed;
 
-  function ERC20Standard(uint256 _totalSupply, string _symbol, string _name, uint8 _decimalUnits) public {
+    	function ERC20Standard(uint256 _totalSupply, string _symbol, string _name, uint8 _decimalUnits) public {
 		decimals = _decimalUnits;
 		symbol = _symbol;
 		name = _name;
 		owner = msg.sender;
-        totalSupply = _totalSupply * (10 ** decimals);
-        balances[msg.sender] = totalSupply;
-  }
+		totalSupply = _totalSupply * (10 ** decimals);
+		balances[msg.sender] = totalSupply;
+    	}
+
 	//Fix for short address attack against ERC20
 	modifier onlyPayloadSize(uint size) {
 		assert(msg.data.length == size + 4);
@@ -33,18 +34,18 @@ contract ERC20Standard {
 
 	function transfer(address _recipient, uint256 _value) onlyPayloadSize(2*32) public {
 		require(balances[msg.sender] >= _value && _value > 0);
-	    balances[msg.sender] -= _value;
-	    balances[_recipient] += _value;
-	    Transfer(msg.sender, _recipient, _value);        
-    }
+	    	balances[msg.sender] -= _value;
+	    	balances[_recipient] += _value;
+	    	Transfer(msg.sender, _recipient, _value);        
+    	}
 
 	function transferFrom(address _from, address _to, uint256 _value) public {
 		require(balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0);
-        balances[_to] += _value;
-        balances[_from] -= _value;
-        allowed[_from][msg.sender] -= _value;
-        Transfer(_from, _to, _value);
-    }
+        	balances[_to] += _value;
+        	balances[_from] -= _value;
+        	allowed[_from][msg.sender] -= _value;
+        	Transfer(_from, _to, _value);
+    	}
 
 	function approve(address _spender, uint256 _value) public {
 		allowed[msg.sender][_spender] = _value;
